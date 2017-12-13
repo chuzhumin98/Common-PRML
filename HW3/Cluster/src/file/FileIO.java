f2bb04be00fdd2b4b689933beea97f9dec994aa6
chuzhumin98 <chuzhumin98@gmail.com>
2017-12-13 18:29:09 +0800

    add for PCA calculate

--- HW3/Cluster/src/file/FileIO.java
+++ HW3/Cluster/src/file/FileIO.java
@@ -89,14 +89,64 @@ public class FileIO {
 		
 	}
 	
+	/*
+	 * 进行PCA操作，并将结果写入personPCA中
+	 */
 	private void doPCA() {
 		EigenvalueDecomposition eigens = this.sigma.eig();
 		Matrix eigenVector = eigens.getV();
+		double[] eigenValue = eigens.getRealEigenvalues();
 		
-		Matrix vector1 = eigenVector.getMatrix(0, this.attriNumInital-1, this.attriNumInital-1, this.attriNumInital-1);
-		eigenVector.print(10, 10);
-		vector1.print(10, 10);
-		this.sigma.times(vector1).print(10, 10);
+		/*
+		 * 输出eigenvalue
+		 */
+		String output = "eigenValues:\n";
+		for (int i = 0; i < eigenValue.length; i++) {
+			output += String.valueOf(i+1)+": "+String.valueOf(eigenValue[i])+"\n";
+		}
+		System.out.println(output);
+		
+		Matrix vector1 = eigenVector.getMatrix(0, this.attriNumInital-1, 
+				this.attriNumInital-1, this.attriNumInital-1); //主特征向量
+		Matrix vector2 = eigenVector.getMatrix(0, this.attriNumInital-1, 
+				this.attriNumInital-2, this.attriNumInital-2); //次特征向量
+		
+		for (int i = 0; i < this.personInital.size(); i++) {
+			double[][] Xtmp = {this.personInital.get(i).attris};
+			Matrix X = new Matrix(Xtmp);
+			double element1 = X.times(vector1).get(0, 0);
+			double element2 = X.times(vector2).get(0, 0);
+			PersonEntry person = new PersonEntry(2);
+			person.attris[0] = element1;
+			person.attris[1] = element2;
+			this.personPCA.add(person);
+		}
+		
+		
+		/*
+		 * 这一段是为了检测计算过程是否出错,在非调试阶段可以注释掉
+		 */
+		double[] thismu = new double [this.attriNumPCA];
+		Matrix thissigma = new Matrix(this.attriNumPCA, this.attriNumPCA);
+		for (int i = 0; i < this.attriNumPCA; i++) {
+			thismu[i] = 0.0; //首先初始化为0
+			for (PersonEntry item: this.personPCA) {
+				thismu[i] += item.attris[i];
+			}
+			thismu[i] /= this.personPCA.size();
+		}
+		for (int i = 0; i < this.attriNumPCA; i++) {
+			for (int j = 0; j < this.attriNumPCA; j++) {
+				double total = 0.0;
+				for (PersonEntry item: this.personPCA) {
+					total += (item.attris[i]-thismu[i]) * (item.attris[j]-thismu[j]);
+				}
+				total /= this.personPCA.size();
+				thissigma.set(i, j, total);
+			}
+		}
+		System.out.println("the sigma matrix of the PCA extract attributes is:");
+		thissigma.print(10, 10);
 		
 		//eigens.getD().print(10, 10);
 	}
