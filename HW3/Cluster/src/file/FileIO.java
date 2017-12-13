b6e710c04226f68f868ddcd49505a5d0b4c30485
chuzhumin98 <chuzhumin98@gmail.com>
2017-12-13 16:48:14 +0800

    “get sigma matrix

--- HW3/Cluster/src/file/FileIO.java
+++ HW3/Cluster/src/file/FileIO.java
@@ -5,14 +5,17 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
+import Jama.Matrix;
+
 
 public class FileIO {
 	public int attriNumInital = 10; //初始特征维数为10
 	public int attriNumPCA = 2; //PCA变换后的特征维数为2(PCA变换后才确定的)
 	public ArrayList<PersonEntry> personInital = new ArrayList<PersonEntry>(); //建立一个所有样本的数组
 	public ArrayList<PersonEntry> personPCA = new ArrayList<PersonEntry>(); //存储PCA变换之后的特征
+	public Matrix sigma = null; //协方差矩阵
+	public double[] mu = null; //均值mu向量 
+	
 	public static FileIO file = null;
 	/*
 	 * 进行实例化，实现单子模式
@@ -28,7 +31,10 @@ public class FileIO {
 	 * 构造函数时导入数据
 	 */
 	FileIO() {
+		mu = new double [this.attriNumInital];
+		this.sigma = new Matrix(this.attriNumInital, this.attriNumInital);
 		try {
+			@SuppressWarnings("resource")
 			Scanner input = new Scanner(new File("input/dataset3.txt"));
 			while (input.hasNextLine()) {
 				String line = input.nextLine();
@@ -41,9 +47,44 @@ public class FileIO {
 				this.personInital.add(person);
 			}
 			System.out.println("the sample size:"+this.personInital.size());
+			this.calculateSigma();
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	/*
+	 * 计算mu均值向量和协方差矩阵
+	 */
+	private void calculateSigma() {
+		/*
+		 * 首先计算mu均值向量
+		 */
+		for (int i = 0; i < this.attriNumInital; i++) {
+			this.mu[i] = 0.0; //首先初始化为0
+			for (PersonEntry item: this.personInital) {
+				this.mu[i] += item.attris[i];
+			}
+			this.mu[i] /= this.personInital.size();
+		}
+		
+		/*
+		 * 在此基础上计算sigma协方差矩阵
+		 */
+		for (int i = 0; i < this.attriNumInital; i++) {
+			for (int j = 0; j < this.attriNumInital; j++) {
+				double total = 0.0;
+				for (PersonEntry item: this.personInital) {
+					total += (item.attris[i]-this.mu[i]) * (item.attris[j]-this.mu[j]);
+				}
+				total /= this.personInital.size();
+				this.sigma.set(i, j, total);
+			}
+		}
+		//System.out.println("following is the sigma matrix:");
+		//this.sigma.print(10, 10);
+		
 	}
 	
 	public static void main(String[] args) {
